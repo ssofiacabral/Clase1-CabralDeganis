fadb7f5c209c5f9a8d66978186b1bcf6ba205c66
ssofiacabral <68235143+ssofiacabral@users.noreply.github.com>
2021-03-20 19:36:41 +0100

    Add files via upload

---
+++ algoritmo_basico.js
@@ -0,0 +1,8 @@
+//Pop up para que usuario haga una acción
+let nombre = prompt('Ingrese su nombre y apellido')
+let edad = prompt('Ingrese su edad')
+let mail = prompt('Ingrese su mail')
+
+// Mensaje
+alert('Bienvenido' + " " + nombre + "."+ " " + 'Usted tiene' + " "+ edad + " " + 'años' + " " + 'y su mail es:' + " " + mail)
+
